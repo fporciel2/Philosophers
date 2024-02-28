1e5eb66e603695c91f4ab8f3c9ca981bf259bc97
fporciel <nomail>
2024-02-28 12:53:22 +0100

    philo_start_simulation.c completed.

--- philo/philo_start_simulation.c
+++ philo/philo_start_simulation.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 12:29:02 by fporciel          #+#    #+#             */
-/*   Updated: 2024/02/28 12:39:17 by fporciel         ###   ########.fr       */
+/*   Updated: 2024/02/28 12:52:11 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* 'Philosophers' is a simulation of a solution to the dining philosophers
@@ -35,9 +35,24 @@
 
 #include "philo.h"
 
+static int	philo_init_mutexes(t_data *data)
+{
+	uint64_t	i;
+
+	i = 0;
+	pthread_mutex_init(&data->stdout_mutex, NULL);
+	while (data->philosophers[i].id)
+	{
+		pthread_mutex_init(&data->philosophers[i].fork, NULL);
+		i++;
+	}
+	return (philo_god(data));
+}
+
 int	philo_start_simulation(t_data *data)
 {
 	data->stdout_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
 	if (!data->stdout_mutex)
 		return (philo_cleaner(data));
+	return (philo_init_mutexes(data));
 }
