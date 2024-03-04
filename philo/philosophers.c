6e4b801d9e33d5b5c1477a945fe1d0c688bfd9ed
fporciel2 <124692932+fporciel2@users.noreply.github.com>
2024-03-04 14:40:33 +0100

    Forks generated. Metaphorically.

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 10:53:45 by fporciel          #+#    #+#             */
-/*   Updated: 2024/03/04 14:25:27 by fporciel         ###   ########.fr       */
+/*   Updated: 2024/03/04 14:34:45 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* 'Philosophers' is a simulation of a solution to the dining philosophers
@@ -44,7 +44,8 @@ static int	philo_initialize(t_input *input, t_gdata *global_data)
 			&global_data->number_of_meals);
 	philo_init_global_data(input, global_data);
 	if (!philo_init_timestamps(input, global_data)
-			|| !philo_init_mutexes(input, global_data))
+			|| !philo_init_mutexes(input, global_data)
+			|| !philo_init_forks(input, global_data))
 		return (0);
 	printf("Initialized global data\n");
 	printf("number_of_philosophers: %lu\n",
