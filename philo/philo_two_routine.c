d37ce016fddef00ecc133b3aa5dee7ba23c489f1
fporciel2 <124692932+fporciel2@users.noreply.github.com>
2024-03-04 11:53:35 +0100

    Developed philo_two_routine.c

--- philo/philo_two_routine.c
+++ philo/philo_two_routine.c
@@ -6,12 +6,12 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:26:21 by fporciel          #+#    #+#             */
-/*   Updated: 2024/03/04 11:17:36 by fporciel         ###   ########.fr       */
+/*   Updated: 2024/03/04 11:53:11 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/* <one line to give the program's name and a brief idea
- * of what it does.>
- * Copyright (C) <year>  <name of author>
+/* 'Philosophers' is a simulation of a solution to the dining philosophers
+ * problem.
+ * Copyright (C) 2024  Federico Porciello
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,56 @@
  * 
  * For more information on how to contact me by electronic and paper mail
  * please see:
- * <your contact information here>
+ * https://github.com/fporciel2/Philosophers
+ *
+ * This part of the program implements the main routine using two philosophers.
  */
 
+#include "philo.h"
 
+static int	philo_two_routine_execution(t_gdata *global_data)
+{
+	if (!philo_will_eat(global_data, global_data->odd_philosophers))
+		return (philo_kill_em_all(global_data, global_data->odd_philosophers));
+	pthread_create(&global_data->odd_philosophers->philosopher,
+			NULL, philo_odd_eat, global_data->odd_philosophers);
+	pthread_join(global_data->even_philosophers->philosopher, NULL);
+	if (!philo_will_eat(global_data, global_data->even_philosophers))
+		return (philo_kill_em_all(global_data, global_data->even_philosophers));
+	pthread_create(&global_data->even_philosophers->philosopher,
+			NULL, philo_even_eat, global_data->even_philosophers);
+	pthread_join(global_data->odd_philosophers->philosopher, NULL);
+	if (!philo_will_sleep(global_data, global_data->odd_philosophers))
+		return (philo_kill_em_all(global_data, global_data->odd_philosophers));
+	pthread_create(&global_data->odd_philosophers->philosopher,
+			NULL, philo_odd_sleep, global_data->odd_philosophers);
+	pthread_join(global_data->even_philosophers->philosopher, NULL);
+	if (!philo_will_sleep(global_data, global_data->even_philosophers))
+		return (philo_kill_em_all(global_data, global_data->even_philosophers));
+	pthread_create(&global_data->even_philosophers->philosopher,
+			NULL, philo_even_sleep, global_data->even_philosophers);
+	pthread_join(global_data->odd_philosophers->philosopher, NULL);
+}
+
+int	philo_two_routine(t_gdata *global_data)
+{
+	uint64_t	number_of_meals;
+
+	number_of_meals = global_data->number_of_times_each_philosopher_must_eat;
+	if (number_of_meals > 0)
+	{
+		while (number_of_meals--)
+		{
+			if (!philo_two_routine_execution(global_data))
+				return (0);
+		}
+	}
+	else
+	{
+		while (1)
+		{
+			if (!philo_two_routine_execution(global_data))
+				return (0);
+		}
+	}
+}
